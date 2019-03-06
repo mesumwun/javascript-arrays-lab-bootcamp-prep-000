e9fc70867d458a94e0f7f4761ff66fc5629fa4ba
Daniel Hope <upinhope@gmail.com>
2019-03-06 23:53:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = [ 'Milo', 'Otis', 'Garfield' ];
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
