884d6778bdd3b692113d26f59b3438840290c2bf
Daniel Hope <upinhope@gmail.com>
2019-03-06 23:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function destructivelyPrependKitten(name) {
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
